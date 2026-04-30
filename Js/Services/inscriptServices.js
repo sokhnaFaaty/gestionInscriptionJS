ca91518767a8a9ef49af489ceb38042df471a41f
nexuusdev <mamefat2004@gmail.com>
2026-04-30 14:13:36 +0200

    Fonctionnalite rechercher une inscription

--- Js/Services/inscriptServices.js
+++ Js/Services/inscriptServices.js
@@ -6,3 +6,25 @@ import { dateFormater } from "../Utiles/datesFormater.js";
 
 // Tableau en mémoire, initialisé depuis le localStorage
 let etudiants = getInscriptions();
+
+
+//rechercher
+function getFiltered() {
+    const inscriptions = getInscriptions();
+    const q = Dom.searchInput.value.toLowerCase().trim(); // Ajoutez .value ici !
+
+    if (!q) return inscriptions;
+
+    return inscriptions.filter(et => {
+        // Liste des champs dans lesquels chercher
+        const fields = [et.nom, et.prenom, et.email, et.adresse, et.telephone, et.formation];
+        
+        return fields.some(field => 
+            field?.toLowerCase().includes(q)
+        );
+    });
+}
+Dom.searchInput.addEventListener("input",function(){
+const filtrer=getFiltered()
+afficherInscriptions(filtrer)
+})
