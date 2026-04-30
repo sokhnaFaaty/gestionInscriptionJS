2e4c6ed8114a0b3275fa267af6f65f39e31f27d1
nexuusdev <mamefat2004@gmail.com>
2026-04-30 17:33:23 +0200

    verif

--- Js/Services/inscriptServices.js
+++ Js/Services/inscriptServices.js
@@ -1,5 +1,5 @@
 import { saveInscriptions, getInscriptions } from "../Stores/inscriptStores.js";
-import * as Dom from "../Dom/elements.js";
+import * as Dom from "../Dom/element.js";
 import { fermerModal } from "../UI/modalRenderer.js";
 import { showToast } from "../UI/messageRenderer.js";
 import { dateFormater } from "../Utiles/datesFormater.js";
