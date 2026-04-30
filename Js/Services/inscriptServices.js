f541842cfa0f76db6f5137933d3e51b5ba36dbbe
nexuusdev <mamefat2004@gmail.com>
2026-04-30 14:07:30 +0200

    tableau etudiants initialis2s depuis le localstorage

--- Js/Services/inscriptServices.js
+++ Js/Services/inscriptServices.js
@@ -4,3 +4,5 @@ import { fermerModal } from "../UI/modalRenderer.js";
 import { showToast } from "../UI/messageRenderer.js";
 import { dateFormater } from "../Utiles/datesFormater.js";
 
+// Tableau en mémoire, initialisé depuis le localStorage
+let etudiants = getInscriptions();
