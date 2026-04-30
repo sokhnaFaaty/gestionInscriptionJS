4400484f0e3b4a3b5bfacbe19553ec748fed3800
nexuusdev <mamefat2004@gmail.com>
2026-04-30 16:12:48 +0200

    ouverture et fermeture modal

--- Js/UI/modalRenderer.js
+++ Js/UI/modalRenderer.js
@@ -1,3 +1,14 @@
 import * as Dom from "../Dom/elements.js";
 import { emailDejaUtilise, modifierEtudiant, supprimerEtudiant, restaurerEtudiant } from "../Services/inscriptServices.js";
 import { showToast } from "./messageRenderer.js";
+
+
+// OUVRIR / FERMER
+export function ouvrirModal(modal) {
+    modal.classList.remove("hidden");
+}
+
+
+export function fermerModal(modal) {
+    modal.classList.add("hidden");
+}
